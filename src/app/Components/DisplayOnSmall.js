b94c494c7ca887a01bd6408a3e6f682dfa6106e7
RoshanCodeJourney <roshanail7613@gmail.com>
2024-05-09 14:07:58 +0500

    Test File Deleted

--- src/app/Components/DisplayOnSmall.js
+++ src/app/Components/DisplayOnSmall.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DisplayOnSmall = () => {
   return (
     <div className='w-full lg:hidden  fixed top-0 h-screen bg-black z-[999] flex items-center justify-center'>
-        <p className='font-bold'>This site is not responsive</p>
+        <p className='font-bold'>This Site Is Only For PC</p>
       
     </div>
   )
